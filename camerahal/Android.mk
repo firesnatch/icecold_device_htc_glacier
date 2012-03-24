946a653d9d3ec6fa328b8a2e273949b4e8ec5da1
firesnatch <sean.neeley@gmail.com>
2012-03-23 22:28:04 -0500

    Added custom camerahal changes

---
+++ camerahal/Android.mk
@@ -0,0 +1,39 @@
+ifeq ($(TARGET_BOARD_PLATFORM),msm7x30)
+
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := cameraHAL.cpp
+LOCAL_C_INCLUDES := $(TOP)/frameworks/base/include
+
+LOCAL_SHARED_LIBRARIES := liblog libutils libcutils
+LOCAL_SHARED_LIBRARIES += libui libhardware libcamera_client
+LOCAL_SHARED_LIBRARIES += libcamera
+LOCAL_PRELINK_MODULE := false
+
+#ifeq ($(BOARD_HAVE_HTC_FFC), true)
+    LOCAL_CFLAGS += -DHTC_FFC
+#endif
+#ifeq ($(BOARD_FFC_HAS_OFFSET), true)
+    LOCAL_CFLAGS += -DOFFSET_FFC
+#endif
+ifeq ($(BOARD_USE_REVERSE_FFC), true)
+    LOCAL_CFLAGS += -DREVERSE_FFC
+endif
+ifeq ($(TARGET_USE_CAF_PARAMS), true)
+    LOCAL_CFLAGS += -DCAF_PARAMS
+endif
+ifeq ($(CAMERA_CONVERT_COLORS),true)
+    LOCAL_CFLAGS += -DCPU_COLOR_CONVERT
+endif
+
+include $(BUILD_SHARED_LIBRARY)
+
+endif
+
